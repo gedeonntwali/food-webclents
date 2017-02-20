a771ec25e66070ffa2c3823eddb008152c05dfcb
Gedeon Ntwali <ntwari28@gmail.com>
2017-02-20 17:40:09 +0200

    I added PORO in my controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,19 +1,18 @@
 class FoodsController < ApplicationController
 
   def index
-    @foods = Unirest.get("http://localhost:3000/api/v2/foods.json").body    
+    @foods = Food.all
   end
 
   def show
-    food_hash = Unirest.get("http://localhost:3000/api/v2/foods/#{params[:id]}.json").body
-    @food = Food.new(food_hash)
+    @food = Food.find(params[:id])
   end
 
   def new
   end
 
   def create
-    @food = Unirest.post("http://localhost:3000/api/v1/employees.json", :headers => {"Accept"=> "application/json"}, :parameters => {:ingredient => params[:ingredient], :spice => params[:spice], :measurement=> params[:measurement]}).body
+    @food = Food.create(params[:ingredient], params[:spice], params[:measurement])
     redirect_to "/foods/#{@food['id']}"
   end
 
@@ -23,14 +22,13 @@ class FoodsController < ApplicationController
 
 
   def update
-    @food = Unirest.patch("http://localhost:3000/api/v2/foods/#{params[:id]}.json", :headers => {"Accept"=> "application/json"}, :parameters => {:ingredient => params[:ingredient], :spice => params[:spice], :measurement=> params[:measurement]}).body
-    render :show
+    @food = Food.update(params[:ingredient], params[:spice], params[:measurement])
+  redirect_to "/foods/#{params[:id]}"
   end
   
   def destroy
-    @food = Food.find_by(id:params[:id])
-    @food.destroy
-    render json: {message: "food deleted!!!!!"}
+    @food = Food.delete(params[:id])
+
   end
 
 end
--- app/models/concerns/food.rb
+++ app/models/concerns/food.rb
@@ -8,4 +8,30 @@ class Food
     @spice = hash['spice']
     @measurement = hash['measurement']
   end
+
+  def self.find(id)
+    food_hash = Unirest.get("http://localhost:3000/api/v2/foods/#{id}.json", :headers => {"Accept"=>"application/json", "X-User-Email"=>"ged@ged.com", "Authorization"=>"Token token=123_cba"}).body
+    @food = Food.new(food_hash)
+  end
+
+  def self.all
+    foods = Unirest.get("http://localhost:3000/api/v2/foods.json", :headers => {"Accept"=>"application/json", "X-User-Email"=>"ged@ged.com", "Authorization"=>"Token token=123_cba"}).body  
+    @foods = []
+      foods.each do |food|
+        @foods << Food.new(food)
+      end
+    return @foods
+  end
+
+  def self.create(ingredient, spice, measurement)
+    Unirest.post("http://localhost:3000/api/v1/employees.json", :headers => {"Accept"=> "application/json"}, :parameters => {:ingredient => params[:ingredient], :spice => params[:spice], :measurement=> params[:measurement]}).body
+  end
+
+  def self.update(ingredient, spice, measurement)
+    Unirest.patch("http://localhost:3000/api/v2/foods/#{id}.json")
+  end
+
+
+
+
 end
